f3bd53f3209f2136abcd85f787dd4c5107760392
thiagotfsilva <thiagophsilva@hotmail.com>
2024-07-02 23:00:09 -0300

    criando teste para outros idiomas e refatorando

--- hello/ola.go
+++ hello/ola.go
@@ -3,14 +3,33 @@ package main
 import "fmt"
 
 const prefixoOla = "Olá, "
+const prefixoOlaEspanhol = "Hola, "
+const prefixoOlaFrances = "Bonjour, "
+const prefixoOlaIngles = "Hello, "
 
-func Ola(nome string) string {
+func Ola(nome, idioma string) string {
 	if nome == "" {
 		nome = "Mundo"
 	}
-	return  prefixoOla + nome
+	
+	return  prefixoDeSaudacao(idioma) + nome
+}
+
+func prefixoDeSaudacao(idioma string) (prefixo string) {
+	switch idioma {
+	case "frances":
+		prefixo = prefixoOlaFrances
+	case "espanhol":
+		prefixo = prefixoOlaEspanhol
+	case "ingles":
+		prefixo = prefixoOlaIngles
+	default: 
+		prefixo = prefixoOla
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Ola("mundo"))
+	fmt.Println(Ola("mundo", ""))
 }
--- hello/ola_test.go
+++ hello/ola_test.go
@@ -12,14 +12,32 @@ func TestOla(t *testing.T) {
 	}
 
 	t.Run("diz olá para as pessoas", func(t *testing.T) {
-		resultado := Ola("Chris")
+		resultado := Ola("Chris", "")
 		esperado := "Olá, Chris"
 		verificaMensagemCorreta(t, resultado, esperado)
 	})
 
 	t.Run("diz 'Olá, mundo' quando uma string vazia for passada", func(t *testing.T) {
-		resultado := Ola("")
+		resultado := Ola("", "")
 		esperado := "Olá, Mundo"
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+
+	t.Run("em espanhol", func(t *testing.T) {
+		resultado := Ola("Elodie", "espanhol")
+		esperado := "Hola, Elodie"
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+
+	t.Run("em francês", func(t *testing.T) {
+		resultado := Ola("Marie", "frances")
+		esperado := "Bonjour, Marie"
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+
+	t.Run("em inglês", func(t *testing.T) {
+		resultado := Ola("John", "ingles")
+		esperado := "Hello, John"
 		verificaMensagemCorreta(t, resultado, esperado)
 	})
 
