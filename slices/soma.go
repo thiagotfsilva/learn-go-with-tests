737207124e607a828ed59764e1317081d3a893d3
thiagotfsilva <thiagophsilva@hotmail.com>
2024-07-03 21:42:52 -0300

    criando test para funcao soma

---
+++ slices/soma.go
@@ -0,0 +1,9 @@
+package slices
+
+func Soma(numeros [5]int) int {
+	soma := 0
+	for _, numero := range numeros {
+		soma += numero
+	}
+	return soma
+}
---
+++ slices/soma_test.go
@@ -0,0 +1,13 @@
+package slices
+
+import "testing"
+
+func TestSoma(t *testing.T) {
+	numeros := [5]int{1, 2, 3, 4, 5}
+	resultado := Soma(numeros)
+	esperado := 15
+
+	if esperado != resultado {
+		t.Errorf("resultado %d, esperado %d, dado %v", resultado, esperado, numeros)
+	}
+}
